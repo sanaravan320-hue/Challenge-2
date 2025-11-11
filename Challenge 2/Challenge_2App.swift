8077363329dd58e5502317bd753119cf5f75a7c7
sanaravan320-hue <sana.ravan320@gmail.com>
2025-11-11 09:55:02 +0100

    Initial Commit

---
+++ Challenge 2/Challenge_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_2App.swift
+//  Challenge 2
+//
+//  Created by Sana Ravan on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge 2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Challenge 2
+//
+//  Created by Sana Ravan on 11/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
